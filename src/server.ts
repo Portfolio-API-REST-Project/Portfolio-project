b5d863454ba063778ef33cfd3a420dadd9494a89
Sablinita <sablinahare@gmail.com>
2022-12-20 12:18:20 +0100

    used alba example

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import router from "./route/route"
 const app = express()
 app.use(cors());
 app.use (router);
-// app.get("/portfolio", (req: Request, res: Response) => {
-//         res.json({ message: "name" })
-//     });
+app.get("/portfolio", (req: Request, res: Response) => {
+        res.json({ message: "name" })
+    });
 app.listen(3008, () => console.log("listening on port 3008"));
